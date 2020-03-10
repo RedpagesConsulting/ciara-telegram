8bda61cc8d6e6de97a73f73d640d1b849e4e1ef4
Edidiong <edidiong.aligbe@gmail.com>
2020-03-10 16:15:34 +0100

    Update app
    Update

--- server.js
+++ server.js
@@ -1,34 +1,13 @@
 const express = require('express');
-const TelegramBot = require('node-telegram-bot-api');
-const OracleBot = require('@oracle/bots-node-sdk');
+const service = require('./service');
+const pkg = require('./package.json');
 
+const logger = console;
 const app = express();
-OracleBot.init(app);
+service(app);
 
- // Telegram bot - webhook config
-const telegramToken = 'ADD_YOUR_TELGRAM_BOT_TOKEN';
-const telegramURL = 'YOUR_TELEGRAM_WEBHOOK_URL';
-const bot = new TelegramBot(telegramToken);
-
-// implement webhook
-const { WebhookClient, WebhookEvent } = OracleBot.Middleware;
-
-const channel = {
-  url: process.env.BOT_WEBHOOK_URL,
-  secret: process.env.BOT_WEBHOOK_SECRET
-};
-const webhook = new WebhookClient({ channel: channel });
-webhook.on(WebhookEvent.ERROR, console.error); // receive errors
-
-// receive bot messages
-app.post('/bot/message', webhook.receiver()); // receive bot messages
-webhook.on(WebhookEvent.MESSAGE_RECEIVED, message => {
-  // format and send to messaging client...
+const server = app.listen(process.env.PORT || 3000, () => {
+  logger.info(`${pkg.name} service online\n`);
 });
 
-// send messages to bot (example)
-app.post('/user/message', (req, res) => {
-  let message = {/* ... */}; // format according to MessageModel
-  webhook.send(message)
-    .then(() => res.send('ok'), e => res.status(400).end());
-});
+module.exports = server;
---
+++ service.js
@@ -0,0 +1,128 @@
+const OracleBot = require('@oracle/bots-node-sdk');
+const TelegramBot = require('node-telegram-bot-api');
+const { WebhookClient, WebhookEvent } = OracleBot.Middleware;
+
+module.exports = (app) => {
+  const logger = console;
+  // initialize the application with OracleBot
+  OracleBot.init(app, {
+    logger,
+  });
+
+  // Telegram bot - webhook config
+  const telegramToken = process.env.TELEGRAM_API_KEY;
+  const telegramURL = 'YOUR_TELEGRAM_WEBHOOK_URL';
+  const bot = new TelegramBot(telegramToken);
+  //bot.setWebHook( telegramURL );
+  // variable to store chatId
+  var chatId;
+  
+  // add webhook integration
+  const webhook = new WebhookClient({
+    channel: {
+        url: process.env.BOT_WEBHOOK_URL,
+        secret: process.env.BOT_WEBHOOK_SECRET
+    }
+  });
+  const MessageModel = webhook.MessageModel();
+  // Add webhook event handlers
+  webhook.on(WebhookEvent.MESSAGE_RECEIVED, message => {
+    logger.info("Message from bot:", message);
+
+    // send to client...
+
+    var cards = [];
+    var keyboard = [];
+    var keyboard3 = [];
+    var idx = 0;
+
+    if (Array.isArray(message.messagePayload.actions)) {
+      for (var i = 0; i < message.messagePayload.actions.length; i++) {
+        logger.info("Button 1-" + message.messagePayload.actions[i].label);
+        // keyboard
+
+        keyboard3.push({ text: message.messagePayload.actions[i].label });
+        
+        idx++;
+       
+        if (idx == 3) {
+          idx = 0;
+          keyboard.push(keyboard3);
+          keyboard3 = [];
+        }
+
+        if(i == 9){
+          keyboard3.push({ text: message.messagePayload.globalActions[0].label });
+        }
+        
+      }
+
+      if (idx != 0) {
+       
+        keyboard.push(keyboard3);
+        
+      }
+      
+    }
+
+   
+    if (Array.isArray(message.messagePayload.cards)) {
+      for (var i = 0; i < message.messagePayload.cards.length; i++) {
+        // cards
+
+        cards.push({ text: message.messagePayload.cards[i].title });
+      }
+    }
+
+    logger.info(JSON.stringify(keyboard));
+    logger.info(message.messagePayload.hasOwnProperty("globalActions"));
+
+    var text = "" + message.messagePayload.text;
+
+    if (keyboard.length > 0) {
+      var opts = {
+        reply_markup: {
+          keyboard: keyboard,
+          resize_keyboard: true,
+          one_time_keyboard: true
+        }
+      };
+
+      bot.sendMessage(chatId, text, opts);
+    } else if (cards.length > 0) {
+      var opts = { reply_markup: { inline_keyboard: [cards] } };
+
+      bot.sendMessage(chatId, text, opts);
+    } else {
+      //var opts = {"reply_markup": { "keyboard": [[]] } };
+
+      bot.sendMessage(chatId, text);
+    }
+  });
+
+  // Send message to bot.
+  bot.on("message", msg => {
+    // set chatId variable for use in sendMessage()
+    logger.info(msg);
+    chatId = msg.chat.id;
+    
+    const message = {
+      userId: "eddy",
+      messagePayload: MessageModel.textConversationMessage(msg.text)
+    };
+    
+    // send to bot webhook channel
+    console.log(message);
+    webhook.send(message);
+  });
+
+  // Create endpoint for bot webhook channel configurtion (Outgoing URI)
+  app.post("/bot/message", webhook.receiver());
+  // Create endpoint for telegram webhook
+  app.post("/bot/telegram", (req, res) => {
+    console.log("123457");
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+
+}
